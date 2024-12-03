269f4cbb2cebaf1b846c825ddfa9382ffcd896a0
arisarisyi <alarisyi@gmail.com>
2024-12-03 17:42:53 +0800

    create pagination

--- src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,17 +1,18 @@
 namespace Catalog.API.Models.Products.GetProducts;
 
 // Best practice selalu define Req dan Response di endpoint
-// public record GetProductsRequest()
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 public record GetProductsResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
-        {
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
+            {
+                var query = request.Adapt<GetProductsQuery>();
             //mediatr send ke handler
-            var result = await sender.Send(new GetProductsQuery());
+            var result = await sender.Send(query);
 
             //mapster map ke GetProductsResponse. pastikan response sama pada handler dan endpoint
             //agar bisa terhubung
